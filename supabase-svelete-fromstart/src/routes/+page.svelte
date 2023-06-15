e1beff97a9a106f1a6c6095233598cfe63da57bb
sunwu <sunwu5678@gmail.com>
2023-06-15 18:14:37 +0900

    dfdf

---
+++ supabase-svelete-fromstart/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import supabase from "$lib/db"
+    import { onMount } from "svelte";
+
+    let todos = [];
+
+    onMount(async () => {
+        let {data, error} = await supabase.from("todos").select("*");
+        todos = data;
+        console.table(todo);
+     })
+</script>
+
+{#each todos as todo}
+    <div>
+        <input
+    </div>
+{:else}
+<p>No todos found</p>
+{/each}
