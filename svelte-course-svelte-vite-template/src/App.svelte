e590e0cf7e9a469fd6f11dd9c3a0c21e665ba3dd
sunwupark <sunwu5678@gmail.com>
2023-06-26 16:40:41 +0900

    go

--- svelte-course-svelte-vite-template/src/App.svelte
+++ svelte-course-svelte-vite-template/src/App.svelte
@@ -1,8 +1,13 @@
 <script>
 	import Button from './lib/Button.svelte';
+	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte';
+	import './app.css'
 </script>
 
-<Button size="small" shadow>Button Text</Button>
+<Button size="small" shadow>
+	<div style:width="20px" slot="leftContent"><FaAngellist /></div>
+	Button Text
+</Button>
 
 <style>
 </style>
--- svelte-course-svelte-vite-template/src/lib/Counter.svelte
+++ svelte-course-svelte-vite-template/src/lib/Counter.svelte
@@ -1,21 +1,21 @@
 <script>
-  export let initialCount = 0;
-  export let maxCount = undefined;
-  let count = initialCount;
+	export let initialCount = 0;
+	export let maxCount = undefined;
 
-  console.log($$props, $$restProps)
+	console.log($$props, $$restProps);
 
-  function increment(){
-    if(count == maxCount) return;
-    count +=1;
-  }
+	let count = initialCount;
 
+	function increment() {
+		if (count === maxCount) return;
+		count += 1;
+	}
 </script>
 
 <button on:click={increment}>Clicks {count}</button>
 
 <style>
-  button {
-    padding: 15px 20px;
-  }
-</style> 
+	button {
+		padding: 15px 20px;
+	}
+</style>
