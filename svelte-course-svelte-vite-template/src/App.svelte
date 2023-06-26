82ca99b566be6f4fc67b60cb83f662aaa15b8616
sunwupark <sunwu5678@gmail.com>
2023-06-26 17:18:46 +0900

    why not finishing

--- svelte-course-svelte-vite-template/src/App.svelte
+++ svelte-course-svelte-vite-template/src/App.svelte
@@ -1,11 +1,19 @@
 <script>
 	import Button from './lib/Button.svelte';
 	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte';
+	import FaAllergies from 'svelte-icons/fa/FaAllergies.svelte';
 	import './app.css'
 </script>
 
-<Button size="small" shadow>
-	<div style:width="20px" slot="leftContent"><FaAngellist /></div>
+<Button let:isLeftHovered size="small" shadow>
+	<div style:width="20px" slot="leftContent" let:x>
+		{x}
+		{#if isLeftHovered}
+			<FaAngellist />
+		{:else}
+			<FaAllergies />
+		{/if}
+	</div>
 	Button Text
 </Button>
 
