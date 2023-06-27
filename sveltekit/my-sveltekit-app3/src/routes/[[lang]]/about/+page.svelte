d8e0410a8717bc14859089b81f2785147f22ec55
sunwupark <sunwu5678@gmail.com>
2023-06-27 10:47:18 +0900

    integermatcher

---
+++ sveltekit/my-sveltekit-app3/src/routes/[[lang]]/about/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { page } from '$app/stores';
+    let name = 'John';
+</script>
+
+<h1>About {name}</h1>
+
+{JSON.stringify($page.params)}
+
+<button on:click={() => {
+    goto('/');
+}}>Go to home</button>
+
+<style>
+    
+</style>
