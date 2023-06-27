e5dc18f3cd84fd43ffc75af0083bffd03471418b
sunwupark <sunwu5678@gmail.com>
2023-06-27 11:44:52 +0900

    dfddfdf

---
+++ sveltekit/my-sveltekit-app3/src/routes/(admin)/settings/general/+page.svelte
@@ -0,0 +1,1 @@
+<h2>Genereal Settings</h2>
---
+++ sveltekit/my-sveltekit-app3/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<div style:background="#f03" style:padding="10px">
+    <h1>Root Layout</h1>
+
+    <slot />
+
+    <footer style:margin-top="60px">
+        All rights reserved. 
+    </footer>
+</div>
+
+
+
